07a837ae0cccccfa7fe85d150e6724ea32143d79
MagnusDevelopr <sudhanshusharma103@gmail.com>
2023-12-14 18:03:09 +0530

    change mongo db url

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://sudhanshubsrdev:vldmYilLRycsa7b1@cluster0.9qqadao.mongodb.net/");
+mongoose.connect(
+  "mongodb+srv://sudhanshubsrdev:8CWBDGJUzl7nuIV4@cluster0.wbtwrdy.mongodb.net/IssueTracker?retryWrites=true&w=majority"
+);
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open",()=>{
-    console.log("Connected to MongoDB");
-})
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 export default db;
